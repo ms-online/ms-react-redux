7aba9eff256fafd53a2aed70939f963bc71d9d5c
ms-online <27732357@qq.com>
2020-10-27 21:38:33 +0800

    lesson-15

--- myapp/src/Contact.js
+++ myapp/src/Contact.js
@@ -1,20 +1,45 @@
 import React from 'react';
 
 const Contact = (props) => {
-        const { contactForm } = props;
-        const formList = contactForm.map(contact => {
-            return (
-                <div className="ContactFrom" key = {contact.id}>
-                <div>{ contact.name }</div>
-                <div>{ contact.age }</div>
-                <div>{ contact.sex }</div>
-            </div>
-            )
-        })
-        // console.log(contactForm);
+    const { contactForm } = props;
+    // if条件语句
+    // const formList = contactForm.map(contact => {
+    //     if (contact.age >= 30) {
+    //     return (
+    //             <div className="ContactFrom" key = {contact.id}>
+    //             <div>{ contact.name }</div>
+    //             <div>{ contact.age }</div>
+    //             <div>{ contact.sex }</div>
+    //         </div>
+    //         )
+    //     } else {
+    //         return null
+    //         }
+    //     })
+
+    //三元运算符
+    // const formList = contactForm.map(contact => {
+    //     return contact.sex === 'Male' ? (
+    //          <div className="ContactFrom" key = {contact.id}>
+    //             <div>{ contact.name }</div>
+    //             <div>{ contact.age }</div>
+    //             <div>{ contact.sex }</div>
+    //        </div>
+    //     ) : null
+    // })
         return (
             <div className="formList">
-                { formList }
+                {
+                 contactForm.map(contact => {
+                        return contact.sex === 'Male' ? (
+                             <div className="ContactFrom" key = {contact.id}>
+                                <div>{ contact.name }</div>
+                                <div>{ contact.age }</div>
+                                <div>{ contact.sex }</div>
+                           </div>
+                        ) : null
+                    })
+                }
             </div>
         )
     }

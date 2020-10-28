ec61eefdf8eed57adcca4aae9ef4c5f37a1f8b49
ms-online <27732357@qq.com>
2020-10-28 22:03:45 +0800

    lesson-24

---
+++ msonline/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import Navbar from './component/Navbar'
+import { Route, BrowserRouter } from 'react-router-dom'
+import Home from './component/Home'
+import About from './component/About'
+import Contact from './component/Contact'
+
+class App extends Component{
+  render() {
+    return (
+       <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/contact' component={Contact}/>
+        </div>
+       </BrowserRouter>
+       
+  );
+  }
+ 
+}
+
+export default App;

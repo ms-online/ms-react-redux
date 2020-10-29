9be72f9ff72a678b0e51570f4a54092e279995d2
ms-online <27732357@qq.com>
2020-10-29 19:26:15 +0800

    lesson-30

--- msonline/src/component/Post.js
+++ msonline/src/component/Post.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Post extends Component {
     state = {
-        id:null
+        post: null
     }
 
     componentDidMount() {
         // console.log(this.props)
         let id = this.props.match.params.post_id;
-        // console.log(id)
-        this.setState({
-            id
-        });
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + id).then(res => {
+            // console.log(res)
+            this.setState({
+                post:res.data
+            })
+        })
+        
     }
     render() {
+
+        const post = this.state.post ? (
+            <div className="post">
+                <h4 className="center">{this.state.post.title}</h4>
+                <p>{this.state.post.body}</p>
+            </div>
+        ): (
+            <div className="container">博客正在加载......</div>
+        )
+
         return (
             <div className="container">
-                <h4>{this.state.id}</h4>
+                <h4>{post}</h4>
             </div>
         )
     }

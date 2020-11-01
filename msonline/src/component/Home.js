1e81f0bbf5d11c9e6d7c7e8089620cd7913cb818
ms-online <27732357@qq.com>
2020-11-01 17:13:21 +0800

    lesson-41

--- msonline/src/component/Home.js
+++ msonline/src/component/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import Pokeball from '../pokeball.png'
 import { connect } from 'react-redux'
+
 // import Rainbow from '../hoc/Rainbow'
 
 

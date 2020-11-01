ca637908e6f1f35dbcf56b4faf19466def4b02c4
ms-online <27732357@qq.com>
2020-11-01 17:14:56 +0800

    lesson-32

--- msonline/src/App.js
+++ msonline/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from './component/Navbar'
-import { Route, BrowserRouter} from 'react-router-dom'
+import { Route, BrowserRouter, Switch} from 'react-router-dom'
 import Home from './component/Home'
 import About from './component/About'
 import Contact from './component/Contact'
@@ -12,11 +12,13 @@ class App extends Component{
        <BrowserRouter>
         <div className="App">
           <Navbar />
+          <Switch>
           <Route exact path='/' component={Home}/>
           <Route path='/about' component={About}/>
           <Route path='/contact' component={Contact} />
           <Route path='/:post_id'
             component={Post} />
+          </Switch>
         </div>
        </BrowserRouter>
        
